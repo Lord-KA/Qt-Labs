f492994a53dddda70906926b9e2c391b7797fdcb
Lord <lord.nemo@protonmail.com>
2021-11-21 15:52:56 +0300

    finished save/open/new dialogs; started Edit menu

--- Lab5/MainWindow.cpp
+++ Lab5/MainWindow.cpp
@@ -4,25 +4,77 @@ MainWindow::MainWindow(QWidget* parent)
 	: QMainWindow(parent)
 {
 	mainMenu     = menuBar()->addMenu(tr("&File"));
-	actionQuit   = mainMenu->addAction(tr("&Quit"),    QCoreApplication::instance(), SLOT(quit()));
+	actionQuit   = mainMenu->addAction(tr("&Quit"),    this, SLOT(quit()));
 	actionOpen   = mainMenu->addAction(tr("&Open"),    this, SLOT(openFile()));
 	actionNew    = mainMenu->addAction(tr("&New"),     this, SLOT(newFile()));
 	actionSave   = mainMenu->addAction(tr("&Save"),    this, SLOT(saveFile()));
 	actionSaveAs = mainMenu->addAction(tr("&Save as"), this, SLOT(saveAsFile()));
+
+    editMenu     = menuBar()->addMenu("&Edit");
+    actionUndo   = editMenu->addAction("Undo",   this, [&, this](){this->area->undo();});
+    actionRedo   = editMenu->addAction("Redo",   this, [&, this](){this->area->redo();});
+    actionCopy   = editMenu->addAction("Copy",   this, [&, this](){this->area->copy();});
+    actionPaste  = editMenu->addAction("Paste",  this, [&, this](){this->area->paste();});
 
     area = new CodeEditor();
     
     setCentralWidget(area);
 
 	setFixedSize(1280, 720);
+
+    setDefaultFilename();
+}
+
+void MainWindow::exitApp()
+{
+    #ifdef EXTRA_VERBOSE
+        std::cerr << "Exiting the application!\n";
+    #endif
+    
+    exit(0);
+}
+
+void MainWindow::quit()
+{
+    #ifdef EXTRA_VERBOSE
+        std::cerr << "Quit Dialog!\n";
+    #endif
+    
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "Exit", "Exit without saving?",
+                                                                QMessageBox::Save   | 
+                                                                QMessageBox::Cancel |
+                                                                QMessageBox::Close    );
+    if (reply == QMessageBox::Save) {
+        saveFile();
+        exitApp();
+    } else if (reply == QMessageBox::Close)
+        exitApp();
+}
+
+void MainWindow::setDefaultFilename()
+{
+    int num = 1;
+    std::ifstream file;
+    do {
+        file.close();
+        filename = DEFAULT_FILENAME + QString::number(num);
+        file.open(filename.toStdString(), std::ifstream::in);
+        ++num;
+    } while (file.good());
 }
 
 void MainWindow::saveFile() 
 {
+    QString text = area->toPlainText();
+
+    if (filename.isEmpty()) {
+        setDefaultFilename();
+    }
+
     #ifdef EXTRA_VERBOSE
         std::cerr << "Saving File to " << filename.toStdString() << "!\n";
     #endif
-    QString text = area->toPlainText();
+
     QFile fout(filename);
     fout.open(QIODevice::ReadWrite | QIODevice::Text);
     fout.write(text.toUtf8());
@@ -32,22 +84,33 @@ void MainWindow::saveFile()
 
 void MainWindow::saveAsFile()
 {
-    #ifdef EXTRA_VERBOSE
-        std::cerr << "Saving File As to " << filename.toStdString() << "!\n";
-    #endif
-}    //TODO
-
-void MainWindow::openFile() 
-{
-    #ifdef EXTRA_VERBOSE
-        std::cerr << "Opening File " << filename.toStdString() << "!\n";
-    #endif
     QFileDialog dialog(this);
     dialog.setFileMode(QFileDialog::AnyFile);
     QStringList fileNames = {};
     if (dialog.exec())
         fileNames = dialog.selectedFiles();
     filename = fileNames[0];
+
+    #ifdef EXTRA_VERBOSE
+        std::cerr << "Saving File As to " << filename.toStdString() << "!\n";
+    #endif
+ 
+    saveFile();
+}    
+
+void MainWindow::openFile() 
+{
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::AnyFile);
+    QStringList fileNames = {};
+    if (dialog.exec())
+        fileNames = dialog.selectedFiles();
+    filename = fileNames[0];
+
+    #ifdef EXTRA_VERBOSE
+        std::cerr << "Opening File " << filename.toStdString() << "!\n";
+    #endif
+ 
     QFile file(filename);
     if (file.open(QIODevice::ReadWrite | QIODevice::Text)) {
         area->setPlainText(file.readAll());
@@ -57,7 +120,10 @@ void MainWindow::openFile()
 
 void MainWindow::newFile()  
 {
+   
+    setDefaultFilename();
+
     #ifdef EXTRA_VERBOSE
-        std::cerr << "Creating new File!\n";
+        std::cerr << "Creating new File " << filename.toStdString() << " !\n";
     #endif
-}      //TODO
+}  
--- Lab5/MainWindow.hpp
+++ Lab5/MainWindow.hpp
@@ -11,30 +11,40 @@
 #include <QString>
 #include <QFileDialog>
 #include <QFile>
+#include <QMessageBox>
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "TextEditor.hpp"
 
+static const char DEFAULT_FILENAME[] = "untitled";
 
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
+private:
+	QMenu *mainMenu, *editMenu;
+    QAction *actionQuit, *actionOpen, *actionSave, *actionNew, *actionSaveAs;
+    QAction *actionUndo, *actionRedo, *actionCopy, *actionPaste;
+    QToolBar *toolbar;
+    CodeEditor *area;
+    QString filename = "";
+
 public:
 	MainWindow(QWidget *parent = nullptr);
 
+private:
+    void setDefaultFilename();
+    void exitApp();
+
 public slots:
+    void quit();
     void openFile();
     void newFile();
     void saveFile();
     void saveAsFile();
-
-private:
-	QMenu *mainMenu;
-    QAction *actionQuit, *actionOpen, *actionSave, *actionNew, *actionSaveAs;
-    QToolBar *toolbar;
-    CodeEditor *area;
-    QString filename = "dump.txt";
 };
+
